48334dde0ee70b5276357b10ff43d42d5d015da7
SMAINI-smail <smaini.smail@gmail.com>
2022-02-15 22:27:13 +0100

    Début code bash WP + Ajout install.log

--- projet6.sh
+++ projet6.sh
@@ -1,1 +1,10 @@
+#Début du script d'automatisation de l'installation de WP
 
+
+
+#Mise a jour du système d'exploitation
+sudo apt update > install.log 2>&1 && sudo apt full-upgrade -y > install.log 2>&1
+echo "mise a jour de l'OS en cours ..."
+echo  
+
+#Installation de apache2 et de ces dépendances
