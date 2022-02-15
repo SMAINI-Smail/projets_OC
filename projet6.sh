760a259b73cd292af20e7345dcd3fb92990c1a1d
SMAINI-smail <smaini.smail@gmail.com>
2022-02-15 22:52:49 +0100

    première ligne de code en bash

--- projet6.sh
+++ projet6.sh
@@ -1,10 +1,12 @@
 #Début du script d'automatisation de l'installation de WP
 
-
-
 #Mise a jour du système d'exploitation
+echo "La mise a jour du système d'exploitation est en cours, Patientez SVP ... "
+echo
 sudo apt update > install.log 2>&1 && sudo apt full-upgrade -y > install.log 2>&1
-echo "mise a jour de l'OS en cours ..."
-echo  
 
-#Installation de apache2 et de ces dépendances
+#Vérification si Apache2 est installé sur le serveur
+if [  ]
+#Installation de apache2
+echo "Installation du serveur web appache en cours ..."
+sudo apt install apache2 -y >> install.log 2>
